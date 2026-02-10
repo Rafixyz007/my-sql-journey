83e0c6a79a7f8efed339b2fc66226de586175969
Md Shakhawat Hossain <122726297+Rafixyz007@users.noreply.github.com>
2026-02-10 13:00:15 +0600

    Update select_queries.sql

--- 01_basics/select_queries.sql
+++ 01_basics/select_queries.sql
@@ -1,1 +1,23 @@
 
+-- ============================================
+-- SQL BASICS: SELECT STATEMENT
+-- ============================================
+
+-- Select all columns from the customers table
+SELECT *
+FROM customers;
+
+-- Select specific columns only
+SELECT
+    first_name,
+    country,
+    score
+FROM customers;
+
+-- Best practice example:
+-- Avoid SELECT * when you only need a few columns
+SELECT
+    customer_id,
+    first_name,
+    last_name
+FROM customers;
